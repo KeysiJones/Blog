41a75bf91b26a224f60b1fc6fc4a232e5de1bcd9
keysi.fernandes <keysi.fernandes@hypeflame.com.br>
2021-08-04 13:32:24 -0300

    feat: reestruturação do projeto, separando front do back

--- backend/kdaily/src/main/java/com/keysijones/kdaily/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.keysijones.kdaily.repository;
-
-import com.keysijones.kdaily.model.Article;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-    
-}
